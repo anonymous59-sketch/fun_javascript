17da788ae400b9d7d913cac7de4dc85a85072741
parksw <home@naver.com>
2025-12-21 12:40:07 +0900

    과제 코드리뷰 후 수정

--- todo/todo_1219.js
+++ todo/todo_1219.js
@@ -23,17 +23,21 @@ let friends = [
 	{name: `박길동`, tel: `010-4444-5555`, birth: `05-17-04`, tall: 154.7}
 ];
 
-let sum = 0, avg = 0, max = 0;
+let sum = 0; // 코드 리뷰를 해보니 변수 선언을 해야 됨
+let max = 0;
+let max_name = ""; // 변수를 하나씩 선언하는게 가독성도 좋고 git으로 확인 했을 때 어느 변수가 수정되었는지 확인하기가 편리함
 for (let prop of friends) {
 	sum += prop.tall;
 	if (max < prop.tall) {
 		max = prop.tall;
-		max_name = `cm(${prop.name})`;
+		max_name = `${prop.name}`;
 	}
 }
-avg = sum / friends.length;
-max += max_name;
-console.log(`키의 평균은 ${avg}cm이고 제일 큰 키는 ${max}이다.`);
+let avg = sum / friends.length; // 요즘 추세는 변수를 상단에 다 선언하는 것이 아니라 필요한 경우나 사용하는 곳 근처에서 선언을 하는 것이다.
+
+// max += max_name // 이건 자료형이 다른 두 변수를 합치는 것이라 권장되지 않음
+
+console.log(`키의 평균은 ${avg}cm이고 제일 큰 키는 ${max}cm(${max_name})이다.`);
 
 let table = `<table border="2px"><thead><tr><th>이름</th><th>전화번호</th>
 <th>생일</th><th>키</th></tr></thead><tbody>`;
@@ -42,6 +46,6 @@ for (let prop of friends) {
 	table += `<tr><td>${prop.name}</td><td>${prop.tel}</td><td>${prop.birth}</td><td>${prop.tall}cm</td></tr>`;
 }
 
-table += `</tbody><tfoot><tr><td colspan="2">키 평균 / 제일 큰 사람</td><td colspan="2">${avg}cm / ${max}</td></tr></tfoot></table>`;
+table += `</tbody><tfoot><tr><td colspan="2">키 평균 / 제일 큰 사람</td><td colspan="2">${avg}cm / ${max}(${max_name})</td></tr></tfoot></table>`;
 
 document.writeln(table);
